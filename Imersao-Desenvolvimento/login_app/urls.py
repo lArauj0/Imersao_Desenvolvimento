7fc5f88d275a44e3ea4da1c03f95cb455abf4bc0
CesarLucas <cesarlucas1206@gmail.com>
2024-11-23 09:49:00 -0300

    Fotos

--- Imersao-Desenvolvimento/login_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('cadastrar/', views.criar_usuario, name='criar_usuario'),
-    path('login/', views.login_usuario, name='login_usuario'),
-    path('logout/', views.logout_usuario, name='logout_usuario'),
-]
